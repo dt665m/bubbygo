9d7476bd91e78ba2368fe5c65e03823aa5161aa0
Denis Tsai <denistsai@blazingorb.com>
2018-03-08 18:29:47 -0800

    update comments and change some naming

--- scheduler.go
+++ scheduler.go
@@ -9,18 +9,18 @@ import (
 )
 
 const (
-	DefaultKeepAlive = 30 * time.Second
+	DefaultKeepAlive = 500 * time.Millisecond
 )
 
 // Scheduler is a bubbygo go routine scheduler
 type Scheduler struct {
 	goRoutines uint64
-	tokens     chan struct{}
+	sem        chan struct{}
 	queue      chan func()
 	keepAlive  time.Duration
 }
 
-// NewScheduler creates a new bubbygo scheduler with default Go Routine KeepAlive Time of 30 minutes
+// NewScheduler creates a new bubbygo scheduler
 func NewScheduler(maxRoutines, queueSize, preStart int) *Scheduler {
 	if preStart <= 0 && queueSize > 0 {
 		panic(fmt.Sprintf("scheduler deadlock settings detected, preStart: %d queueSize: %d", preStart, queueSize))
@@ -30,25 +30,25 @@ func NewScheduler(maxRoutines, queueSize, preStart int) *Scheduler {
 	}
 	s := &Scheduler{
 		goRoutines: 0,
-		tokens:     make(chan struct{}, maxRoutines),
+		sem:        make(chan struct{}, maxRoutines),
 		queue:      make(chan func(), queueSize),
 		keepAlive:  time.Duration(DefaultKeepAlive),
 	}
 
 	for i := 0; i < preStart; i++ {
-		s.tokens <- struct{}{}
-		go s.process(func() {}, nil)
+		s.sem <- struct{}{}
+		go s.process(func() {}, false)
 	}
 
 	return s
 }
 
-// GoRoutineCount gives an estimate of the number of go routines active.  Do not use as form of synchronization
+// GoRoutineCount gives an estimate of the number of go routines active.  Do not use for thread synchronization
 func (s *Scheduler) GoRoutineCount() int {
 	return int(s.goRoutines)
 }
 
-// SetKeepAlive duration used to set the keepAlive time of generated go routines
+// SetKeepAlive sets the keepAlive time of generated go routines
 func (s *Scheduler) SetKeepAlive(duration time.Duration) {
 	s.keepAlive = duration
 }
@@ -62,31 +62,41 @@ func (s *Scheduler) Do(ctx context.Context, job func()) error {
 		return ctx.Err()
 	case s.queue <- job:
 		return nil
-	case s.tokens <- struct{}{}:
-		go s.process(job, time.After(s.keepAlive))
+	case s.sem <- struct{}{}:
+		go s.process(job, true)
 		return nil
 	}
 }
 
 // process handles the job closure passed in and dequeues jobs off the Scheduler.Queue until
 // Scheduler.keepAlive duration.  process will stay alive indefinitely if keepAlive is a nil channel
-func (s *Scheduler) process(job func(), keepAlive <-chan time.Time) {
-	//handle the job that started this go routine
+func (s *Scheduler) process(job func(), willExpire bool) {
+	// handle the job that started this go routine
 	atomic.AddUint64(&s.goRoutines, 1)
 	job()
 
-	//try to process more jobs before finishing, since spinning up go routines aren't free
+	// try to process more jobs before finishing, since spinning up go routines aren't free
+	var expire *time.Timer
+	if willExpire {
+		expire = time.NewTimer(s.keepAlive)
+	}
 RecycleGoRoutine:
 	for {
 		select {
 		case job := <-s.queue:
 			job()
-		case <-keepAlive:
+
+			// avoid race condition where reset can still observe previous in-flight message
+			if !expire.Stop() {
+				<-expire.C
+			}
+			expire.Reset(s.keepAlive)
+		case <-expire.C:
 			break RecycleGoRoutine
 		}
-
 	}
-	<-s.tokens
 
+	// finish and release semaphore
+	<-s.sem
 	atomic.AddUint64(&s.goRoutines, ^uint64(0))
 }
