071fe5ee82cbe8d0e35351f7927678f14c7e31f3
Denis Tsai <denistsai@blazingorb.com>
2018-03-09 20:39:21 -0800

    renaming

--- scheduler.go
+++ scheduler.go
@@ -53,9 +53,9 @@ func (s *Scheduler) SetKeepAlive(duration time.Duration) {
 	s.keepAlive = duration
 }
 
-// Do consumes a token slot in s.tokens and starts a go routine to process the job if s.token isn't full.
-// If s.tokens channel is full, the job is pushed into the s.queue until s.queue is full
-// If both s.tokens and s.queue are full, Do will block on ctx.Done()
+// Do consumes a semaphore slot in s.sem and starts a go routine to process the job if s.sem isn't full.
+// If s.sem channel is full, the job is pushed into s.queue until s.queue is full
+// If both s.sem and s.queue are full, Do will block on ctx.Done()
 func (s *Scheduler) Do(ctx context.Context, job func()) error {
 	select {
 	case <-ctx.Done():
@@ -69,30 +69,30 @@ func (s *Scheduler) Do(ctx context.Context, job func()) error {
 }
 
 // process handles the job closure passed in and dequeues jobs off the Scheduler.Queue until
-// Scheduler.keepAlive duration.  process will stay alive indefinitely if keepAlive is a nil channel
-func (s *Scheduler) process(job func(), willExpire bool) {
+// Scheduler.keepAlive duration if permanent is false, otherwise process will spin indefinitely
+func (s *Scheduler) process(job func(), permanent bool) {
 	// handle the job that started this go routine
 	atomic.AddUint64(&s.goRoutines, 1)
 	job()
 
-	// try to process more jobs before finishing, since spinning up go routines aren't free
-	var expire *time.Timer
-	if willExpire {
-		expire = time.NewTimer(s.keepAlive)
+	// try to process more jobs before finishing, since spinning up a go routine isn't free
+	var expiry *time.Timer
+	if permanent {
+		expiry = time.NewTimer(s.keepAlive)
 	}
-RecycleGoRoutine:
+DoQueueJobs:
 	for {
 		select {
 		case job := <-s.queue:
 			job()
 
 			// avoid race condition where reset can still observe previous in-flight message
-			if !expire.Stop() {
-				<-expire.C
+			if !expiry.Stop() {
+				<-expiry.C
 			}
-			expire.Reset(s.keepAlive)
-		case <-expire.C:
-			break RecycleGoRoutine
+			expiry.Reset(s.keepAlive)
+		case <-expiry.C:
+			break DoQueueJobs
 		}
 	}
 
